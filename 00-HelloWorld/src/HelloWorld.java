56133a27508a662acf36215723b33fa0f7df733c
SVFBr <sheilafreire.br@gmail.com>
2016-08-30 22:23:39 -0300

    Day 0: Hello, World.

---
+++ 00-HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		// Print the hello world and welcome messages to the console
+		System.out.println("Hello World.");
+		System.out.println("Welcome to 30 Days of Code.");
+	}
+
+}
